eb2591e0828e53f9453943f6e8ec6af0edee4429
Yael <yaelechols@gmail.com>
2022-12-08 20:27:36 -0800

    Updates App to render ClassInfo component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import StudentList from './components/StudentList';
+import ClassInfo from './components/ClassInfo';
 
 function App() {
   return (
     <main>
       <h1>Attendance</h1>
+      <ClassInfo></ClassInfo>
       <StudentList></StudentList>
     </main>
   );
