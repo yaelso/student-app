b79f9267580c042f1328a1a4d460dcb61bcc239d
Yael <yaelechols@gmail.com>
2022-12-08 21:38:33 -0800

    Updates styling for StudentList component

--- src/components/StudentList.js
+++ src/components/StudentList.js
@@ -4,8 +4,8 @@ import './StudentList.css';
 const StudentList = () => {
     return (
         <section>
-            <h2>Student List</h2>
-            <ul>
+            <h2 className="student-list__heading">Student List</h2>
+            <ul className="student-list">
                 <li>Student A</li>
                 <li>Student B</li>
                 <li>Student C</li>
