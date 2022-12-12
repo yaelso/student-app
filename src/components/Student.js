13f2c8c6285154dc0bc1c99896da72874142fbd6
Yael <yaelechols@gmail.com>
2022-12-12 13:20:48 -0800

    Creates Student component

---
+++ src/components/Student.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Student = () => {
+    return (
+        <ul>
+            <li>Nickname: Ada</li>
+            <li>Email: ada@dev.org</li>
+        </ul>
+    );
+}
+
+export default Student;
