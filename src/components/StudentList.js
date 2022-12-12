e7bae1e790f94c36df0d82ed73f64551a76ba7c5
Yael <yaelechols@gmail.com>
2022-12-12 13:24:41 -0800

    Imports and implements Student component in StudentList

--- src/components/StudentList.js
+++ src/components/StudentList.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import './StudentList.css';
+import Student from './Student';
 
 const StudentList = () => {
     return (
         <section>
             <h2 className="student-list__heading">Student List</h2>
             <ul className="student-list">
-                <li>Student A</li>
-                <li>Student B</li>
-                <li>Student C</li>
+                <li><Student /></li>
+                <li><Student /></li>
+                <li><Student /></li>
             </ul>
         </section>
     );
