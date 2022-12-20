0e01d372b1cbf88c541c7309c7eed519117983ee
Yael <yaelechols@gmail.com>
2022-12-19 22:29:34 -0800

    Implements state and event handling for changes to form fields in NewStudentForm component

--- src/components/NewStudentForm.js
+++ src/components/NewStudentForm.js
@@ -1,15 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const NewStudentForm = () => {
+    const [formFields, setFormFields] = useState({
+        name: '',
+        email: '',
+    });
+
+    const onNameChange = (event) => {
+        setFormFields({
+            ...formFields,
+            name: event.target.value
+        })
+    };
+
+    const onEmailChange = (event) => {
+        setFormFields({
+            ...formFields,
+            email: event.target.value
+        })
+    };
+    
     return (
         <form>
             <div>
                 <label htmlFor='fullName'>Name:</label>
-                <input name='fullName' />
+                <input 
+                    name='fullName' 
+                    value={formFields.name} 
+                    onChange={onNameChange} />
             </div>
             <div>
                 <label htmlFor='email'>Email:</label>
-                <input name="email" />
+                <input 
+                    name="email" 
+                    value={formFields.email} 
+                    onChange={onEmailChange} />
             </div>
             <input 
                 type='submit' 
